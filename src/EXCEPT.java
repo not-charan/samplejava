66e39f4f83c8de84ebe9c9d786f72ee32869043e
sricharan-1309 <chintalasricharan2004@gmail.com>
2023-04-28 16:29:42 +0530

    pull except

--- src/EXCEPT.java
+++ src/EXCEPT.java
@@ -1,2 +1,34 @@
-package PACKAGE_NAME;public class EXCEPT {
+import java.util.Scanner;
+
+public class EXCEPT {
+    public static void main(String[] args) {
+
+        Scanner input =new Scanner(System.in);
+        int a =input.nextInt();
+        if(a<=99){
+            try {
+                throw new Excepts();
+            }
+            catch (Exception e){
+                System.out.println(e);
+                System.out.println(e.getMessage());
+                e.printStackTrace();
+            }
+
+        }
+
+    }
+}
+ class Excepts extends Exception {
+    public String toString(){
+        return  "Iam from to string";  //overide
+
+    }
+
+    public String getMessage()
+    {
+        return "Iam from get message"; // override
+    }
+
+
 }
--- src/arraylist.java
+++ src/arraylist.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class arraylist {
+import java.util.ArrayList;
+public class arraylist {
+    public static void main(String[] args) {
+        ArrayList<Integer> number= new ArrayList<Integer>();
+        number.add(1);
+        number.add(2);
+        number.add(2,4);
+        number.set(0,5);
+        number.remove(0);
+        System.out.println(number.size());
+        System.out.println(number);
+        number.clear();
+        System.out.println(number);
+    }
 }
+//Array list,LINKED LIST,HASH MAP,HASH SET
