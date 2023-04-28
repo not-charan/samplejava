b814fb464a4e69a6fc6494943fd8e5283cb5707a
sricharan-1309 <chintalasricharan2004@gmail.com>
2023-04-28 17:12:17 +0530

    Arraylist

--- src/arraylist.java
+++ src/arraylist.java
@@ -1,16 +1,26 @@
 import java.util.ArrayList;
+import java.util.HashSet;
+
 public class arraylist {
     public static void main(String[] args) {
         ArrayList<Integer> number= new ArrayList<Integer>();
+        ArrayList<Integer> numb =new ArrayList<Integer>();
+        HashSet<ArrayList> collection =new HashSet<>();
+
         number.add(1);
         number.add(2);
-        number.add(2,4);
-        number.set(0,5);
-        number.remove(0);
-        System.out.println(number.size());
-        System.out.println(number);
-        number.clear();
-        System.out.println(number);
+        numb.add(12);
+        numb.add(22);
+        number.add(21);
+        number.add(27);
+        numb.add(123);
+        number.add(2223);
+        collection.add(numb);
+        collection.add(number);
+        System.out.println(collection);
+
+
     }
 }
+
 //Array list,LINKED LIST,HASH MAP,HASH SET
---
+++ src/iterrablile.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class iterrablile {
+    public static void main(String[] args) {
+        Deque<String> dq
+                = new ArrayDeque<String>();
+
+
+        dq.addFirst("Chintala");
+        dq.addLast("charan");
+       dq.add("sri");
+
+        System.out.println(dq.size());
+    System.out.println(dq);
+        String a =dq.removeLast();
+        String b =dq.removeFirst();
+        System.out.println("First name is " +b);
+
+    }
+}
